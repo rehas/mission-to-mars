c477e0d41e035022a88b19f7a02ad87480eff6cf
RehaS <beratson@gmail.com>
2019-06-23 15:55:54 +0200

    Basic test for simulation executed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Mars");
 
+        testForFile();
+
+    }
+
+    public static void testForFile(){
+        Simulation sm = new Simulation();
+
+        File test1 = new File("/Users/reha/Documents/playground/MissionToMars/src/resources/phase-1.txt");
+
+        ArrayList<Item> a = sm.loadItems(test1);
+
+        for (Item i : a){
+            System.out.println(i.name + " " + Integer.toString( i.weight));
+        }
     }
 }
