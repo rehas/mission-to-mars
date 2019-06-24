bc1841fd4584d695617aa10a69988810cc7f8f9b
RehaS <beratson@gmail.com>
2019-06-24 23:41:01 +0200

    Simulations created, refactoring needed

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ public class Main {
 
         System.out.println("Hello, Mars");
 
-        testForFile();
+//        testForFile();
 
+        RunForPhaseU1(1);
+        RunForPhaseU1(2);
+
+        RunForPhaseU2(1);
+        RunForPhaseU2(2);
     }
 
     public static void testForFile(){
@@ -21,13 +26,72 @@ public class Main {
             System.out.println(i.name + " " + Integer.toString( i.weight));
         }
 
-        ArrayList<Rocket> U1Fleet = sm.loadU1(a);
+        ArrayList<U1> U1Fleet = sm.loadU1(a);
 
         for (Rocket u1 : U1Fleet){
 
             System.out.println(u1.currentWeight + " " + u1.maxWeight);
         }
 
+        float totalCostU1 = sm.runSimulation(U1Fleet);
+        float totalCostU11 = sm.runSimulation(U1Fleet);
+
+        System.out.println(totalCostU1);
+        System.out.println(totalCostU11);
+
 
     }
+
+    public static void RunForPhaseU1(int phase){
+        Simulation sm = new Simulation();
+
+        File test1 = new File("/Users/reha/Documents/playground/MissionToMars/src/resources/phase-"+phase+".txt");
+
+        ArrayList<Item> a = sm.loadItems(test1);
+
+//        for (Item i : a){
+//            System.out.println(i.name + " " + Integer.toString( i.weight));
+//        }
+
+        ArrayList<U1> U1Fleet = sm.loadU1(a);
+
+        for (Rocket u1 : U1Fleet){
+
+            System.out.println(u1.currentWeight + " " + u1.maxWeight);
+        }
+
+        float totalCostU1 = sm.runSimulation(U1Fleet);
+        float totalCostU11 = sm.runSimulation(U1Fleet);
+
+        System.out.println(totalCostU1);
+        System.out.println(totalCostU11);
+    }
+
+    public static void RunForPhaseU2(int phase){
+        Simulation sm = new Simulation();
+
+        File test1 = new File("/Users/reha/Documents/playground/MissionToMars/src/resources/phase-"+phase+".txt");
+
+        ArrayList<Item> a = sm.loadItems(test1);
+
+//        for (Item i : a){
+//            System.out.println(i.name + " " + Integer.toString( i.weight));
+//        }
+
+        ArrayList<U2> U2Fleet = sm.loadU2(a);
+
+        for (Rocket u2 : U2Fleet){
+
+            System.out.println(u2.currentWeight + " " + u2.maxWeight);
+        }
+
+        float totalCostU1 = sm.runSimulation(U2Fleet);
+        float totalCostU11 = sm.runSimulation(U2Fleet);
+
+        System.out.println(totalCostU1);
+        System.out.println(totalCostU11);
+    }
+
+
+
 }
